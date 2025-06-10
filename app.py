d55e5a706a4f5afcbc205fa3833068d9219a6b28
EY <156153346+cyyau3@users.noreply.github.com>
2025-06-10 00:36:55 -0400

    Commented out Flask dev server in app.py

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ def delete_expense(id):
         return "An error occurred while deleting the expense.", 500
     return redirect('/')
 
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+# if __name__ == "__main__":
+#     with app.app_context():
+#         db.create_all()
+#     app.run(host='0.0.0.0', port=5000, debug=True)
