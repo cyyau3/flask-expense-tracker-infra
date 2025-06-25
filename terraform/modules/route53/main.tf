3ad17799dd80ac6d4af8a0d45dc5ce4c7cb84848
EY <156153346+cyyau3@users.noreply.github.com>
2025-06-24 20:56:37 -0400

    update Route53 A record alias name for DNS to work

--- terraform/modules/route53/main.tf
+++ terraform/modules/route53/main.tf
@@ -5,7 +5,7 @@ resource "aws_route53_record" "app" {
   type    = "A"
 
   alias {
-    name                   = var.alb_dns_name
+    name                   = "dualstack.${var.alb_dns_name}"
     zone_id                = var.alb_zone_id
     evaluate_target_health = true
   }
